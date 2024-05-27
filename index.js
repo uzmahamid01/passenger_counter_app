31a76c558fd546335356bbbcb44aee89eade869d
uzmahamid01 <uzma_hamid@tamu.edu>
2024-05-27 14:15:14 -0500

    fixed

--- index.js
+++ index.js
@@ -3,19 +3,17 @@
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
+let count = 0
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
 
-console.log(countEl)
-
-let count = 0
+// console.log(countEl)
 
 function increment() {
     count += 1 
     countEl.innerText = count
 
 }
-increment()
 
 // 1. Create a function, save(), which logs out the count when it's called
 function save() {
